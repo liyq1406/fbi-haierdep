f87444e1fb25b8d093f5c50299eb1aa36b7ee869
izhangxiaobo <izhangxiaobo@gmail.com>
2014-11-14 13:45:28 +0800

    delete test file

--- test/java/JavaSETest.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lenovo on 2014-11-11.
- */
-public class JavaSETest {
-    public static void main(String[] args) {
-        String str = "¹þ¹þ";
-        System.out.println(str.getBytes().length);
-    }
-}
