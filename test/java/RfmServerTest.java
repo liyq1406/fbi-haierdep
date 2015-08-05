e381431983b149b1a060e8853a9baec161d80127
hanjianlong <hanjianlong@rongshangsoft.com>
2015-08-05 14:33:20 +0800

    提交房地产测试中心

---
+++ test/java/RfmServerTest.java
@@ -0,0 +1,116 @@
+import org.apache.commons.lang.StringUtils;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by Lichao.W At 2015/7/24 8:54
+ * wanglichao@163.com
+ */
+public class RfmServerTest {
+    public static void main(String args[]) throws IOException {
+        int port = 9876;
+        ServerSocket server = new ServerSocket(port);
+        while (true) {
+            Socket socket = server.accept();
+            //每接收到一个Socket就建立一个新的线程来处理它
+            new Thread(new Task(socket)).start();
+        }
+    }
+
+    /**
+     * 用来处理Socket请求的
+     */
+    static class Task implements Runnable {
+
+        private Socket socket;
+
+        public Task(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                handleSocket();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * 跟客户端Socket进行通信
+         * @throws Exception
+         */
+        private void handleSocket() throws Exception {
+            Reader reader = new InputStreamReader(socket.getInputStream());
+            char chars[] = new char[64];
+            int len;
+            StringBuilder sb = new StringBuilder();
+            String temp;
+            int index;
+            if ((len=reader.read(chars)) != -1) {
+                temp = new String(chars, 0, len);
+                sb.append(temp);
+            }
+            String strSBstrSB=sb.toString().replace("|", "").substring(6, 10);
+            String rtnmsg;
+            if("1001".equals(strSBstrSB)){
+                rtnmsg = "000531|0000|1000000000000009|"+
+                        StringUtils.rightPad("prePerName", 255, " ")+"|" +
+                        StringUtils.rightPad("preProAddr", 128, " ")+"|" +
+                        StringUtils.rightPad("preProName", 128, " ");
+            }else if("1002".equals(strSBstrSB)){
+                rtnmsg = "000020|0000|1000000000000009|";
+            }else if("2001".equals(strSBstrSB)){
+                rtnmsg = "000020|0000|0|"+
+                        StringUtils.rightPad("23400", 20, " ")+"|" +
+                        "jianguanzhanghao12345678900014|" +
+                        StringUtils.rightPad("jianguanzhanghao12345678900014户名", 150, " ")+"|"+
+                        "1000000000000009";
+            }else if("2002".equals(strSBstrSB)){
+                rtnmsg = "000020|0000|1000000000000009|";
+            }else if("2011".equals(strSBstrSB)){
+                rtnmsg = "000020|0000|1000000000000009|";
+            }else if("2101".equals(strSBstrSB)){
+                rtnmsg = "000873|0000|jianguanzhanghao12345678900014|"+
+                        StringUtils.rightPad("jianguanzhanghao12345678900014户名", 150, " ")+"|" +
+                        StringUtils.rightPad("20800001", 20, " ")+"|" +
+                        StringUtils.rightPad("shoukuanyinhang", 90, " ")+"|" +
+                        "shoukuanzhanghao12345678900014|" +
+                        StringUtils.rightPad("shoukuanzhanghao12345678900014户名", 150, " ")+"|" +
+                        StringUtils.rightPad("项目名称", 128, " ")+"|" +
+                        StringUtils.rightPad("开发企业名称", 255, " ")+"|" +
+                        "1000000000000009";
+            }else if("2102".equals(strSBstrSB)){
+                rtnmsg = "000020|0000|1000000000000009";
+            }else if("2111".equals(strSBstrSB)){
+                rtnmsg = "000020|0000|1000000000000009";
+            }else if("2201".equals(strSBstrSB)){
+                rtnmsg = "000585|0000|jianguanzhanghao12345678900014|"+
+                        StringUtils.rightPad("jianguanzhanghao12345678900014户名", 150, " ")+"|" +
+                        StringUtils.rightPad("20800001", 20, " ")+"|" +
+                        StringUtils.rightPad("yezhuxingming", 80, " ")+"|" +
+                        StringUtils.rightPad("zhengjianleixing", 30, " ")+"|" +
+                        StringUtils.rightPad("开发企业名称", 255, " ")+"|" +
+                        "1000000000000009";
+            }else if("2202".equals(strSBstrSB)){
+                rtnmsg = "000020|0000|1000000000000009|";
+            }else if("2211".equals(strSBstrSB)){
+                rtnmsg = "000020|0000|1000000000000009|";
+            }else if("2501".equals(strSBstrSB)){
+                rtnmsg = "000067|0000|0|pingtaijizhang12|jianguanyinhangjizhangliushui1|1000000000000009|";
+            }else{
+                rtnmsg = "1234567890111213141516171819202122232425262728293031323334353637383940414243454647484950";
+            }
+            System.out.println("返回报文：" + rtnmsg);
+            Writer writer = new OutputStreamWriter(socket.getOutputStream(),"GB2312");
+            writer.write(rtnmsg);
+            writer.flush();
+            writer.close();
+            reader.close();
+            socket.close();
+        }
+
+    }
+}
