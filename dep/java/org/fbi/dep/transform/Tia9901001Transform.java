95b6e651a22985f20d0deaa52bf54c0c87274617
baiqg <baiyang_1212@163.com>
2015-09-16 08:56:15 +0800

    字符串长度取得修改

--- dep/java/org/fbi/dep/transform/Tia9901001Transform.java
+++ dep/java/org/fbi/dep/transform/Tia9901001Transform.java
@@ -47,7 +47,7 @@ public class Tia9901001Transform extends AbstractTiaTransform {
                 ToolUtil.rightPad(tia9901001Para.body.BRANCH_ID, 30, ' ')+"|"+
                 ToolUtil.rightPad(tia9901001Para.header.USER_ID, 30, ' ')+"|"+
                 ToolUtil.rightPad(tia9901001Para.body.INITIATOR, 1, ' ')+"|";
-        Integer intStrRtnLength = strRtn.length();
+        Integer intStrRtnLength = ToolUtil.length(strRtn);
         strRtn= StringUtils.leftPad(intStrRtnLength.toString(),6, '0')+strRtn;
         return strRtn;
     }
